d7fd3a0aea2d664af24a289d164faa987ab32eb4
Jay McCowan <jaymccowan13@gmail.com>
2024-09-25 13:38:25 -0400

    scaffold project, connect websocket to backend

---
+++ src/lib/utils.ts
@@ -0,0 +1,9 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+import io from 'socket.io-client'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const socket = io('wss://test.duresscrew.box.ca')
